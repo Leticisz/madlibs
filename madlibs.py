5c0234667eb93787e6fda868093914e87152658b
Leticia Sousa <110980928+Leticisz@users.noreply.github.com>
2022-10-24 10:43:03 -0300

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+adj = input("adjetivo: ")
+verb1 = input("verbo: ")
+verb2 = input("verbo: ")
+personagem_famoso = input("Personagem famoso: ")
+
+madlib = f"programação de computador é assim {adj}! Isso me deixa tão animado o tempo todo porque \
+Eu amo {verb1}. fique hidratado e {verb2} como se fosse {personagem_famoso}!"
+
+print(madlib)
